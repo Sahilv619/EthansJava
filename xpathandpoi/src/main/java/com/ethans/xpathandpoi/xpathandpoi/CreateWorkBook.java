49c0d0f4baa47199addb568e0f544bc75886eb1a
Sahil Vyas <43811740+Sahilv619@users.noreply.github.com>
2018-10-03 20:47:06 +0530

    Add files via upload

---
+++ xpathandpoi/src/main/java/com/ethans/xpathandpoi/xpathandpoi/CreateWorkBook.java
@@ -0,0 +1,25 @@
+package com.ethans.xpathandpoi.xpathandpoi;
+import java.io.*;
+import org.apache.poi.xssf.usermodel.*;
+
+public class CreateWorkBook {
+   public static void main(String[] args)throws Exception {
+      //Create Blank workbook
+      XSSFWorkbook workbook = new XSSFWorkbook();
+      
+      XSSFSheet spreadsheet = workbook.createSheet("Create Work Book");
+      
+      
+      
+      
+      
+
+      //Create file system using specific name
+    FileOutputStream out = new FileOutputStream(new File("G:\\createworkbook.xlsx"));
+
+      //write operation workbook using file out object 
+      workbook.write(out);
+      out.close();
+      System.out.println("createworkbook.xlsx written successfully");
+   }
+}
